85fffb4ca2da712cf0979c151642dca7d0fc9c43
rrr-core <rr.ramos.dev@gmail.com>
2024-01-15 10:02:49 -0500

    update gruvbox repository

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -15,21 +15,17 @@
 -- 	end,
 -- }
 
-
 return {
-    'morhetz/gruvbox',
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other start plugins
+    'ellisonleao/gruvbox.nvim',
+	lazy = false,
+	priority = 1000,
 	config = function()
-		-- require("catppuccin").setup({
-		-- 	flavour = "mocha", -- latte, frappe, macchiato, mocha
-		-- 	transparent_background = false,
-		-- 	integrations = {
-		-- 		telescope = true,
-		-- 		harpoon = true,
-		-- 	},
-		-- })
-		-- vim.cmd([[colorscheme catppuccin]])
+        -- require("gruvbox").setup({
+        --     overrides = {
+        --         Normal = {bg = "#000000"}
+        --     }
+        -- })
+        -- vim.o.background = "dark"
 		vim.cmd([[colorscheme gruvbox]])
 	end,
 }
